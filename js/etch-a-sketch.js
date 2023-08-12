4a173dbfe2b939eba4629e2c2acb791eb14bd779
Jess247 <jesss8692@hotmail.com>
2023-08-12 16:12:07 +0200

    Update random color function to generate color of div box

--- js/etch-a-sketch.js
+++ js/etch-a-sketch.js
@@ -1,6 +1,8 @@
 const container =  document.querySelector('.grid-container');
 const generateGridBtn = document.querySelector('.generate-grid-btn');
 const clearGridBtn = document.querySelector('.clear-grid-btn');
+const randomColorBtn = document.querySelector('.rand-color-btn');
+
 
 generateGridBtn.addEventListener('click', () => {
     const userInputNum = prompt("Enter the grid size: ");
@@ -16,6 +18,23 @@ clearGridBtn.addEventListener('click', () => {
     gridItems.forEach((item) => {item.style.backgroundColor = '';});
 });
 
+randomColorBtn.addEventListener('click', () => {
+    const gridItems = document.querySelectorAll('.grid-item');
+    gridItems.forEach((item) => {item.addEventListener('mouseover',(e) => {
+        let color = generateRandomColor();
+        console.log(color),
+        e.target.style.backgroundColor = color;
+    });})
+});
+
+function generateRandomColor(){
+    let maxVal = 0xFFFFFF; // 16777215
+    let randomNumber = Math.random() * maxVal; 
+    randomNumber = Math.floor(randomNumber);
+    randomNumber = randomNumber.toString(16);
+    let randColor = randomNumber.padStart(6, 0);   
+    return `#${randColor.toUpperCase()}`;
+}
 
 function generateGrid(size=16) {
     const containerSize = 530;
@@ -27,11 +46,13 @@ function generateGrid(size=16) {
         div.classList.add('grid-item');
         div.style = `width: ${boxSize}px; height: ${boxSize}px;`;
         div.addEventListener('mouseover',(e) => {
-           e.target.style.backgroundColor = 'pink';
+            if (div.classList.contains('rand-color')){
+                let color = generateRandomColor();
+                console.log(color),
+                e.target.style.backgroundColor = color;
+            }
+           e.target.style.backgroundColor = 'black';
         });
-        // div.addEventListener('mouseout',(e) => {
-        //     e.target.style.backgroundColor = '';
-        //  });
         div.classList.add('grid-item');
         container.appendChild(div);
     }
